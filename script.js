845762b23fe43c735e93a26865f3c6b152d61f3e
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-03 17:09:14 -0500

    completed assignment

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+// array formatted as follows [state abbr as string, annual revenue, number of locations]
+function State(stateIn){
+  this.state = stateIn[0];
+  this.revenue = stateIn[1];
+  this.locationCount = stateIn[2];
+  this.averageRevenue = function(){
+    return (this.revenue / this.locationCount);
+  };
+}
 // object instances
-
+var illinois = new State(["IL", 5000, 12]);
+var minnesota = new State (["MN", 300, 3]);
+var nevada = new State(["NV", 25000, 1]);
 // push object instances to locations array
-
+locations.push(illinois,minnesota,nevada);
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
   locations.forEach(function (element, index) {
